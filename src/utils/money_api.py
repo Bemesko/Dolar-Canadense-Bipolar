f7de9de2b762a37a9876e7bbca89e43442295bf3
Bernardo Mesko <bemesko@gmail.com>
2021-03-14 18:10:42 -0300

    Solicitando itens da API de dinheiro

---
+++ src/utils/money_api.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class MoneyApi():
+    def __init__(self):
+        pass
+
+    def request_money(self):
+        resp = requests.get('https://economia.awesomeapi.com.br/json/all/CAD')
+        if resp.status_code != 200:
+            # This means something went wrong.
+            raise Exception  # ('GET /tasks/ {}'.format(resp.status_code))
+        for todo_item in resp.json().items():
+            print(todo_item)
+
+
+if __name__ == "__main__":
+    money = MoneyApi()
+    money.request_money()
