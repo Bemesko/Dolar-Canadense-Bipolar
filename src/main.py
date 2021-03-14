b06dc05a3f328d3c676291ae29b498eb35fec1c9
Bernardo Mesko <bemesko@gmail.com>
2021-03-14 17:44:15 -0300

    Primeiro Tweet!

--- src/main.py
+++ src/main.py
@@ -7,11 +7,15 @@ auth = tweepy.OAuthHandler(auth_tokens.TWITTER_API_KEY,
 auth.set_access_token(auth_tokens.TWITTER_ACCESS_TOKEN,
                       auth_tokens.TWITTER_ACCESS_TOKEN_SECRET)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True,
+                 wait_on_rate_limit_notify=True)
 
 try:
     api.verify_credentials()
     print("Authentication OK")
+
+    api.update_status("Olá Twitter!")
+    print("Tweet Enviado!")
 except Exception as e:
     print("Error during authentication")
     raise e
