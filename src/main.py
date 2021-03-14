88a89fb091382323d316e466583a6d8911e046ed
Grégori Sabel <seriosabel57@gmail.com>
2021-03-14 19:24:36 -0300

    Tweets.json

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ import utils.twitter_handler as twitter_handler
 
 # Authenticate to Twitter
 tweepy_auth = tweepy.OAuthHandler(auth_tokens.TWITTER_API_KEY,
-                                  auth_tokens.TWITTER_API_KEY)
+                                  auth_tokens.TWITTER_API_KEY_SECRET)
 tweepy_auth.set_access_token(auth_tokens.TWITTER_ACCESS_TOKEN,
-                             auth_tokens.TWITTER_ACCESS_TOKEN)
+                             auth_tokens.TWITTER_ACCESS_TOKEN_SECRET)
 
 tweepy_api = tweepy.API(tweepy_auth, wait_on_rate_limit=True,
                         wait_on_rate_limit_notify=True)
@@ -17,7 +17,7 @@ try:
     tweepy_api.verify_credentials()
     print("Authentication OK")
 
-    tweepy_api.update_status("Tweet com emoji ��")
+    tweepy_api.update_status("Sorry")
     print("Tweet Enviado!")
 except tweepy.error.TweepError as tweepError:
     print("Error during authentication")
