e538082bd99320bcd8c1a1259826b514f8e12ad8
Grégori Sabel <seriosabel57@gmail.com>
2021-03-14 21:35:12 -0300

    frases para quotes e formato do tweet

--- src/utils/twitter_handler.py
+++ src/utils/twitter_handler.py
@@ -1,5 +1,8 @@
 import tweepy.error
 import json
+import emoji
+
+
 
 class TwitterHandler():
     def __init__(self, auth, api):
@@ -19,9 +22,29 @@ class TwitterHandler():
             raise tweepError
 
     def tweet_dollar_price(self, dollar_info):
-        self.api.update_status(f"""{subiu} Dolar canadense: {dollar_info['ask']} às {dollar_info['check_time']}  
-        {self.tweets_reactions['good'][0]}
-        """)
+        if float(dollar_info['pctChange']) > 0:
+            rised = "Subiu!"
+            reaction = self.tweets_reactions["bad"]["quotes"][0]
+            emoji_reaction = self.tweets_reactions["bad"]["emojis"][2]
+        else:
+            rised = "Caiu!"
+            reaction = self.tweets_reactions["good"]["quotes"][0]
+            emoji_reaction = self.tweets_reactions["good"]["emojis"][2]
+
+        price = round(float(dollar_info['ask']), 2) 
+        self.api.update_status((emoji.emojize(f"""{rised} {emoji_reaction} - R$ {price} às {dollar_info['check_time']}  
+        
+{reaction}
+        """, use_aliases= True)))
+
+
+#         texto = ":smile:"
+#         print((emoji.emojize(f"""{rised} {texto} Dolar canadense: {dollar_info['ask']} às {dollar_info['check_time']}  
+# {reaction}
+#         """, use_aliases= True)))
+
+
+#         print(emoji.emojize("oi :hushed:", use_aliases= True))
 
 
 
